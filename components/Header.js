72a08e0cec7a27e231c9b89ac3fbd95198712451
Emy Ifeora <emyboybeats@gmail.com>
2023-02-21 17:01:12 +0100

    ..

--- components/Header.js
+++ components/Header.js
@@ -74,173 +74,25 @@ export default function Header() {
                       <li className="menu-item current-menu-item manu-item-has-children">
                         <a href="#">Home</a>
                         <i className="fa  pq-submenu-icon"></i>
-                        <ul className="sub-menu">
-                          <li className="menu-item">
-                            <a href="main-home.html" aria-current="page">
-                              Main Home
-                            </a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="home-modern.html">home modern</a>
-                          </li>
-                          <li className="menu-item current-menu-item">
-                            <a href="start-up-home.html">Start Up Home</a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="it-agency.html">IT Agency</a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="software-company.html">Software Company</a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="home-classic.html">home classic</a>
-                          </li>
-                        </ul>
                       </li>
                       <li className="menu-item">
                         <a href="#">Services</a>
                         <i className="fa  pq-submenu-icon"></i>
-                        <ul className="sub-menu">
-                          <li className="menu-item">
-                            <a href="software-development.html">
-                              Software Development
-                            </a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="web-development.html">Web Development</a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="mobile-development.html">
-                              Mobile Development
-                            </a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="app-development.html">App Development</a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="qa-testing.html">QA Testing</a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="product-design.html">Product Design</a>
-                          </li>
-                        </ul>
+                        
                       </li>
                       <li className="menu-item">
                         <a href="#">Pages</a>
                         <i className="fa  pq-submenu-icon"></i>
-                        <ul className="sub-menu">
-                          <li className="menu-item">
-                            <a href="about-us.html">About Us</a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="services.html">Our Services</a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="our-team.html">Our Team</a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="leadership.html">leadership</a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="pricing-plan.html">Pricing Plan</a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="faq.html">Faq</a>
-                          </li>
-                          <li className="menu-item">
-                            <a href="404.html">404</a>
-                          </li>
-                        </ul>
                       </li>
                       <li className="menu-item">
                         <a href="#">Portfolio</a>
                         <i className="fa  pq-submenu-icon"></i>
-                        <ul className="sub-menu">
-                          <li className="menu-item menu-item-has-children">
-                            <a href="#">Types</a>
-                            <i className="fa  pq-submenu-icon"></i>
-                            <ul className="sub-menu">
-                              <li className="menu-item">
-                                <a href="standard.html">Standard</a>
-                              </li>
-                              <li className="menu-item">
-                                <a href="masonry.html">Masonry</a>
-                              </li>
-                              <li className="menu-item">
-                                <a href="masonry-joined.html">Masonry Joined</a>
-                              </li>
-                            </ul>
-                          </li>
-                          <li className="menu-item menu-item-has-children">
-                            <a href="#">Columns</a>
-                            <i className="fa  pq-submenu-icon"></i>
-                            <ul className="sub-menu">
-                              <li className="menu-item">
-                                <a href="2-columns.html">2 columns</a>
-                              </li>
-                              <li className="menu-item">
-                                <a href="3-columns.html">3 columns</a>
-                              </li>
-                              <li className="menu-item">
-                                <a href="3-columns-wide.html">3 columns Wide</a>
-                              </li>
-                              <li className="menu-item">
-                                <a href="4-columns-wide.html">4 columns Wide</a>
-                              </li>
-                            </ul>
-                          </li>
-                          <li className="menu-item">
-                            <a href="portfolio-single.html">Portfolio Single</a>
-                          </li>
-                        </ul>
+                        
                       </li>
                       <li className="menu-item">
                         <a href="#">Blog</a>
                         <i className="fa  pq-submenu-icon"></i>
-                        <ul className="sub-menu">
-                          <li className="menu-item menu-item-has-children">
-                            <a href="#">Grid Style</a>
-                            <i className="fa  pq-submenu-icon"></i>
-                            <ul className="sub-menu">
-                              <li className="menu-item">
-                                <a href="one-column-blog.html">
-                                  One Column Blog
-                                </a>
-                              </li>
-                              <li className="menu-item">
-                                <a href="two-column-blog.html">
-                                  Two Column Blog
-                                </a>
-                              </li>
-                              <li className="menu-item">
-                                <a href="three-column-blog.html">
-                                  Three Column Blog
-                                </a>
-                              </li>
-                            </ul>
-                          </li>
-                          <li className="menu-item menu-item-has-children">
-                            <a href="#">Blog Sidebar</a>
-                            <i className="fa  pq-submenu-icon"></i>
-                            <ul className="sub-menu">
-                              <li className="menu-item">
-                                <a href="blog-right-sidebar.html">
-                                  Blog Right Sidebar
-                                </a>
-                              </li>
-                              <li className="menu-item">
-                                <a href="blog-left-sidebar.html">
-                                  Blog Left Sidebar
-                                </a>
-                              </li>
-                            </ul>
-                          </li>
-                          <li className="menu-item">
-                            <a href="app-promotes-sales-profits-in-it-solutions.html">
-                              Blog Single
-                            </a>
-                          </li>
-                        </ul>
+                        
                       </li>
                       <li className="menu-item">
                         <a href="contact-us.html">Contact Us</a>
