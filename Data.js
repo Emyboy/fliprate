7c783b1366257655db084ad4fd6079da40d119e5
olawoyin Daniel <olawoyindaniel95@gmail.com>
2023-09-08 08:12:55 +0100

    added social links

--- Data.js
+++ Data.js
@@ -3,6 +3,9 @@ export const TeamData = [
         name: "Debola Ajayi",
         img: "/assets/images/team/debola.jpeg",
         title: "Managing Partner, Fliprate",
+        social: {
+            linkedin: "https://www.linkedin.com/in/debola-ajayi",
+        },
     },
     {
         name: "Martin Mbonu",
@@ -13,11 +16,17 @@ export const TeamData = [
         name: "Anita Epundu",
         img: "/assets/images/team/anita.jpeg",
         title: "Chief Marketing Officer",
+        social: {
+            linkedin: " https://www.linkedin.com/in/anita-epundu-2996801517",
+        },
     },
     {
         name: "Abdullahi Mohamud Botan",
         img: "/assets/images/team/botan.jpeg",
         title: "Chief Innovation Officer",
+        social: {
+            linkedin: "https://www.linkedin.com/in/abdullahi-mohamud-57b5961a2",
+        },
     },
 ];
 
